ac3b0252f4d5873af736b8de893fa9b2355854fe
admin <admin@192.168.234.129>
2021-06-18 19:34:21 +0200

    Introduzione a PreparedStatement

--- src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -1,32 +1,42 @@
 package it.polito.tdp.esempioSQL.model;
 
-import java.util.List;
 
-import it.polito.tdp.esempioSQL.db.BabsDAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LeggiBabs {
-	
-	public void run() {
-		
-		BabsDAO dao = new BabsDAO() ;
-		
-		List<Station> tutte = dao.listStation() ;
-		
-		for(Station s: tutte) {
-			System.out.println(s.getName()) ;
-		}
-		
-		System.out.println("----") ;
-		List<Station> paloAlto = dao.listStationByLandmark("Palo Alto") ;
-		for(Station s: paloAlto) {
-			System.out.println(s.getName()) ;
-		}
 
+	public void run() {
+		String jdbcURL = "jdbc:mysql://localhost/babs?user=root&password=root";
+		try {
+			Connection connection = DriverManager.getConnection(jdbcURL);	
+			String sql = "SELECT NAME FROM station WHERE landmark = ?";
+			
+			PreparedStatement st = connection.prepareStatement(sql);
+			st.setString(1, "Palo Alto");
+			ResultSet res = st.executeQuery();
+			while(res.next()) {
+				String nomeStazione = res.getString("name");
+				System.out.println(nomeStazione);
+			}
+			st.close();
+		
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 	
 	public static void main(String args[]) {
-		LeggiBabs babs = new LeggiBabs() ;
+		LeggiBabs babs = new LeggiBabs();
 		babs.run();
 	}
-
 }
